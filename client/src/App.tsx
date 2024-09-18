4416462609f3e1ddfc6cf79d427cee86f4820a5e
Miika Reijonen <hmire02@xamk.fi>
2024-09-18 11:09:27 +0300

    1.4 task done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [taskList, setTaskList] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState<string>('');
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  // Handle new task creation, by adding it into the list
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    if (!newTask) return;
+    setTaskList([...taskList, newTask]);
+    setNewTask('');
+  }
+
   return (
     <>
-      <div className="container"></div>
+      <div className="header">
+        <h1>WhatToDo</h1>
+      </div>
+      <div className="container">
+        <form>
+          <input
+            value={newTask}
+            placeholder="New task"
+            onChange={handleChange}
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Add new task
+          </button>
+        </form>
+        <ul>
+          {taskList.map((task) => (
+            <li>{task}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
